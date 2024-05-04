011d618fadedc87c027387a149863c1b2754968c
raphael <raphael.hoarau@epitech.eu>
2024-05-04 17:32:51 +0400

    web2

--- mon-projet/src/composent/Navigation.js
+++ mon-projet/src/composent/Navigation.js
@@ -1,6 +1,15 @@
 import React from "react";
 import { NavLink} from "react-router-dom";
 
+const Accueil = () => {
+  return (
+    <div className="carousel">
+      <h1 style={{ fontFamily: 'Lato, sans-serif'}} className="titre_accueil">Les vétérants du 24h By Webcup</h1>
+      <h2 style={{ fontFamily: 'Lato, sans-serif'}} className="texte_accueil">Durant 24h chrono, les participants vont se challenger autour d'un concours qui consiste à réaliser un site web complet <br></br> à partir d'un sujet qui ne leur sera communiqué qu'au top départ du concours.</h2>
+    </div>
+  );
+};
+
 const HomeLink = () => {
   return (
     <NavLink to="/" className="nav-link-home">
@@ -42,6 +51,7 @@ const Navigation = () => {
         <RejoindreLink />
         <ActusLink />
       </ul>
+      <Accueil />
     </div>
   );
 };
